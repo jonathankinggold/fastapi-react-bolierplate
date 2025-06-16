8f4a5858d25b3b7cdcd35b2056fcde78f91e349d
JonathanKing <redditbetit98@gmail.com>
2025-06-17 00:26:37 +0900

    chore: #4 Upgrade yarn to v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
   server: { host: true },
   envDir: '../',
   envPrefix: ['UI_'],
