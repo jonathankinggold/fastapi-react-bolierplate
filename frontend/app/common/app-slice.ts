8d8795da9524020f3a23c6ca94ef903349591610
JonathanKing <redditbetit98@gmail.com>
2025-06-21 08:27:20 +0900

    chore: #7 include language configuration in app state & initialize i18n

--- frontend/app/common/app-slice.ts
+++ frontend/app/common/app-slice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import i18n from 'i18next'
 
 import { getEnv } from '~/common/utils/env'
 import type { RootState } from '~/store'
@@ -6,6 +7,7 @@ import type { RootState } from '~/store'
 export interface Configuration {
   name: string
   url: string
+  language: string
   api: string
 }
 
@@ -19,9 +21,10 @@ export interface AppState {
 
 const initialState: AppState = {
   conf: {
-    name: getEnv('APP_NAME') as string,
-    url: getEnv('APP_UI_URL') as string,
-    api: getEnv('APP_API_URL') as string,
+    name: getEnv('UI_NAME') as string,
+    url: getEnv('UI_URL') as string,
+    language: getEnv('UI_LANGUAGE') as string,
+    api: getEnv('UI_API') as string,
   },
   isLoading: true,
 }
@@ -30,6 +33,20 @@ export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
+    /**
+     * Initializes the application state by setting the language configuration.
+     *
+     * This function retrieves the language preference from local storage and
+     * updates the application's state accordingly. It also triggers a change in the
+     * language for the i18n instance to ensure proper localization.
+     *
+     * @param {Object} state - The current application state object to be updated.
+     */
+    initState: (state) => {
+      const lang = localStorage.getItem('language') as string
+      state.conf.language = lang
+      void i18n.changeLanguage(lang)
+    },
     loading: (state) => {
       state.isLoading = true
     },
@@ -41,6 +58,6 @@ export const appSlice = createSlice({
 
 export const selectIsLoading = (state: RootState) => state.app.isLoading
 
-export const { loading, loadComplete } = appSlice.actions
+export const { initState, loading, loadComplete } = appSlice.actions
 
 export default appSlice.reducer
