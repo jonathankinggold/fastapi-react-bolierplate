96905047efdc013c4e0bd2d5b47ea158fa5d8f7b
JonathanKing <redditbetit98@gmail.com>
2025-07-07 11:17:51 +0900

    feat: #22 Preparing for Changes to the Frontend Project Structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
-import { reactRouter } from '@react-router/dev/vite'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: { host: true },
-  envDir: '../',
-  envPrefix: ['UI_'],
+  plugins: [react()],
 })
